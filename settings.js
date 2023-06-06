1f4e15c0679355e07dad58ea09be902f329e0e77
Nathanopg <113868907+Nathanopg@users.noreply.github.com>
2023-06-06 12:12:00 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -8,7 +8,7 @@ const {
 
 //sebagian panel gk support opena ai jadi mungkin apikey lu bakalan di blokir
 //klo misalkan pada saat memakai fitur ai, terus dapat email dari open si, itu berarti website tempat lu install bot gk support ai
-global.OpenAIKey = "YOUR_KEY" //login to https://platform.openai.com/account/api-keys to get apikey
+global.OpenAIKey = "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" //login to https://platform.openai.com/account/api-keys to get apikey
 
 // Website Api (jgn di ganti tomlol)
 global.APIs = {
@@ -18,7 +18,7 @@ global.APIs = {
 //buy apikey premium 0887435047326
 // Free apikey (silahkan login terus ganti V12 dgn apikey lu)
 global.APIKeys = {
-   'https://api.zeeoneofc.my.id': 'V12', // 👉 login https://api.zeeoneofc.my.id to get apikey
+   'https://api.zeeoneofc.my.id': 'KZDeTqEiSM8gPDl', // 👉 login https://api.zeeoneofc.my.id to get apikey
 }
 global.sewabotnya = `
 sewabot pc wa.me/6281937399424
